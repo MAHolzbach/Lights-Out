2fc59042fb37f33f701cfc85342be71af40476bb
MAHolzbach <maholzbach@gmail.com>
2017-05-30 16:01:48 -0400

    game logic finished

--- scripts.js
+++ scripts.js
@@ -9,18 +9,36 @@ const init = () => {
       let square2 = document.getElementById(parseInt(e.target.id) + 5);
       let square3 = document.getElementById(e.target.id - 1);
       let square4 = document.getElementById(parseInt(e.target.id) + 1);
+
       e.target.classList.toggle('gray');
+
+      if(e.target.classList.contains('plusException')) {
+        square4.classList.remove('gray');
+      } else if (square4 === null) {
+        console.log(null);
+      } else {
+        square4.classList.toggle('gray');
+      }
+
+      if(e.target.classList.contains('minusException')) {
+        square3.classList.remove('gray');
+      } else if (square3 === null) {
+        console.log(null);
+      } else {
+        square3.classList.toggle('gray');
+      }
+
       if(square1 === null) {
-        
+        console.log(null);
       } else {
         square1.classList.toggle('gray');
       }
-      square2.classList.toggle('gray');
-      square3.classList.toggle('gray');
-      square4.classList.toggle('gray');
-      console.log(e.target.id);
-      console.log('-------');
-      console.log(square2);
+
+      if(square2 === null) {
+        console.log(null);
+      } else {
+        square2.classList.toggle('gray');
+      }
     });
   }
 };
